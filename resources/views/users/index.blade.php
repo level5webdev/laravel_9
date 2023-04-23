69e6d459fc94dbada4f0482d6d15e088b1bd47b4
Luiz Carlos <hidy63@gmail.com>
2023-04-23 18:36:01 -0300

    feat: Add layout at pages views

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,11 +1,16 @@
-<h1>Listagem de usuários</h1>
+@extends('layouts.app')
+@section('title', 'Listagem de usuários')
+@section('content')
+    <h1>Listagem de usuários</h1>
 
-<ul>
-    @foreach ($users as $user)
-        <li>
-            {{ $user->name }}
-            {{ $user->email }}
-            || <a href="{{ route('users.show', $user->id) }}">Vem usuário</a>
-        </li>
-    @endforeach
-</ul>
+    <ul>
+        @foreach ($users as $user)
+            <li>
+                {{ $user->name }}
+                {{ $user->email }}
+                || <a href="{{ route('users.show', $user->id) }}">Vem usuário</a>
+            </li>
+        @endforeach
+    </ul>
+
+@endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Informação do usúario')
+@section('content')
+
 <h1> Seja bem indo {{ $user->name }} </h1>
 
 <ul>
   <li>{{ $user->name}}</li>
   <li>{{ $user->email}}</li>
 </ul>
+
+@endsection
