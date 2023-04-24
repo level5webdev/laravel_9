622155551c5dfa87db314baa53787b097d038bb2
Luiz Carlos <hidy63@gmail.com>
2023-04-23 21:39:33 -0300

    feat: Add link new user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', 'Listagem de usuários')
 @section('content')
-    <h1>Listagem de usuários</h1>
+    <h1>Listagem de usuários ( <a href="{{ route('users.create') }}">+ Criar um novo usúario</a>)</h1>
 
     <ul>
         @foreach ($users as $user)
