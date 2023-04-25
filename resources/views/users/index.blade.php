b343b98d1f8979514bcb12b409ef3ee3ade52f97
Luiz Carlos <hidy63@gmail.com>
2023-04-25 05:42:15 -0300

    feat: Add link show user and edit user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,12 +3,17 @@
 @section('content')
     <h1>Listagem de usuários ( <a href="{{ route('users.create') }}">+ Criar um novo usúario</a>)</h1>
 
+    <form action="{{route('users.index')}}" method="get">
+        <input type="text" name="search" placeholder="Pesquisar" >
+        <button type="submit">Pesquisar</button>
+    </form>
     <ul>
         @foreach ($users as $user)
             <li>
                 {{ $user->name }}
                 {{ $user->email }}
                 || <a href="{{ route('users.show', $user->id) }}">Vem usuário</a>
+                || <a href="{{ route('users.edit', $user->id) }}">Editar usúario</a>
             </li>
         @endforeach
     </ul>
