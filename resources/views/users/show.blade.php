1b1e975a9a2c0c420bb21bc7c6f8e0768476d09d
Luiz Carlos <hidy63@gmail.com>
2023-04-25 05:41:23 -0300

    feat: Add button delete

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,11 +3,17 @@
 @section('title', 'Informação do usúario')
 @section('content')
 
-<h1> Seja bem indo {{ $user->name }} </h1>
+    <h1> Seja bem indo {{ $user->name }} </h1>
 
-<ul>
-  <li>{{ $user->name}}</li>
-  <li>{{ $user->email}}</li>
-</ul>
+    <ul>
+        <li>{{ $user->name }}</li>
+        <li>{{ $user->email }}</li>
+    </ul>
+
+    <form action="{{ route('users.destroy', $user->id) }}" method="post">
+        @method('DELETE') 
+        @csrf
+        <button type="submit ">Deletar</button>
+    </form>
 
 @endsection
